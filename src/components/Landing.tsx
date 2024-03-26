e2392b79316013ea7dfc01051c92eb87f577f458
Guido Rann <guidorann.code@gmail.com>
2024-03-26 14:09:46 -0300

    Primer version de presentacion

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,25 +7,30 @@ import Navbar from "./Navbar";
 export default function Landing() {
   return (
     <main className="font-poppins bg-[#FFF6E0] text-[#61677A]">
-      <section className="bg-[url('/images/hero.jpg')] bg-center bg-cover h-[350px] flex flex-col drop-shadow-xl">
-        <div className="lg:hidden">
+      {/* ------------------------ Seccion principal ---------------------------- */}
+      <section className="bg-[url('/images/hero.jpg')] bg-center bg-cover h-[350px] lg:h-[700px] flex flex-col drop-shadow-xl">
+        <div>
           <Navbar />
         </div>
-        <div className="flex flex-col w-2/3 p-4 justify-center text-white h-full">
-          <p className="text-[35px] font-semibold leading-10 pt-[1px] ">
+        <div className="flex flex-col w-2/3 lg:w-[60%] p-4 justify-center lg:block lg:pt-16 text-white h-full">
+          <p className="text-[35px] lg:text-[95px] lg:px-28 font-semibold lg:leading-[105px] leading-10 pt-[1px] ">
             Entregamos felicidad y creamos
-            <p className="font-imperial text-[70px] leading-8 text-yellow-100">
+            <p className="font-imperial text-[70px] lg:text-[180px] leading-8 lg:leading-[85px] text-yellow-100">
               recuerdos
             </p>
           </p>
         </div>
       </section>
-      <section className="p-4 py-10" id="servicios">
+      {/* --------------------- Presentacion sericios --------------------------- */}
+      <section
+        className="p-4 py-10 lg:h-[700px] lg:flex lg:items-center lg:justify-center"
+        id="servicios"
+      >
         <div className="flex flex-col items-center">
-          <h3 className="text-xl py-2 font-bold font-syne">
+          <h3 className="text-xl lg:text-4xl lg:pb-12 py-2 font-bold font-syne">
             Nuestros servicios
           </h3>
-          <div className="lg:flex lg:gap-4">
+          <div className="lg:flex lg:gap-8">
             <ServiceCards
               img="/images/servicios/globos.jpg"
               serviceName="Globos"
@@ -49,36 +54,41 @@ export default function Landing() {
           </div>
         </div>
       </section>
+      {/* ---------------------- Presentacion personal ------------------------- */}
       <section
-        className="flex flex-col justify-center items-center p-12 bg-[url('/images/presen.jpg')] bg-center bg-cover h-80"
+        className="flex flex-col justify-center items-center p-12 bg-[url('/images/presen.jpg')] bg-center bg-cover h-80 lg:h-[700px]"
         id="nosotros"
       >
-        <div className="border-2 border-white rounded-2xl p-1">
-          <div className="flex flex-col justify-around h-32 items-center px-6 bg-white p-2 rounded-2xl">
-            <h3 className="text-xl font-bold font-syne">Hola! Soy Maira</h3>
+        <div className="border-2 lg:border-[3px] border-white rounded-2xl p-1 lg:p-2">
+          <div className="flex flex-col justify-around lg:justify-evenly h-36 lg:h-[320px] items-center px-6 bg-white p-2 rounded-2xl">
+            <h3 className="text-xl lg:text-4xl font-bold font-syne">
+              Hola! Soy Maira
+            </h3>
             <p className="text-center">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             </p>
           </div>
         </div>
       </section>
-      <section className="px-6 py-14" id="contacto">
-        <div className="bg-[#FEEBBB] rounded-[70px] py-6 flex flex-col justify-between items-center">
+      {/* -------------------------- Seccion Contacto ------------------------- */}
+      <section className="px-6 py-14 lg:py-28 lg:px-[230px]" id="contacto">
+        <div className="bg-[#FEEBBB] rounded-[70px] lg:h-[230px] py-6 flex flex-col justify-between items-center">
           <h1 className="px-6 py-2 text-4xl text-center font-bold font-syne">
             ¡Organiza tu fiesta ya!
           </h1>
-          <p className="text-center pb-6">Empezá a planearlo hoy mismo</p>
-          <button className="flex flex-row justify-center items-center gap-3 p-3 bg-white text-[#128C7E] rounded-full font-semibold">
+          <p className="text-center pb-6 lg:pb-4 lg:text-lg">Empezá a planearlo hoy mismo</p>
+          <button className="flex flex-row justify-center items-center gap-2 p-3 bg-white text-[#128C7E] rounded-full font-semibold">
             Whatsapp
             <IoLogoWhatsapp size={25} />
           </button>
         </div>
       </section>
+      {/* -------------------------- Carrousel Momentos ------------------------- */}
       <section className="p-3 pb-12 flex flex-col items-center" id="momentos">
-        <h3 className="pb-6 text-xl font-bold font-syne">
+        <h3 className="pb-6 text-xl lg:text-4xl font-bold font-syne">
           Galería de Momentos
         </h3>
-        <div className="w-full h-72 lg:h-[500px] lg:w-[900px]">
+        <div className="w-full h-72 lg:h-[700px] lg:w-[1200px]">
           <CarouselCustomNavigation />
         </div>
       </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,27 +4,52 @@ import { useState } from "react";
 export default function Navbar() {
   const [isOpen, setOpen] = useState<boolean>(false);
   return (
-    <div className="flex flex-col text-white rounded-full absolute w-full p-2">
-      <Hamburger toggled={isOpen} toggle={setOpen} />
-      <div className="font-bold w-full">
-        {isOpen ? (
-          <div className="font-poppins text-lg transition-all duration-500 bg-white text-gray-800 h-[200px] flex flex-col justify-between rounded-xl px-5 py-2">
-            <a href="#nosotros" className="h-1/3 flex items-center">
-              <p>Nosotros</p>
+    <div>
+      <div className="flex flex-col text-white absolute rounded-full w-full p-2 lg:hidden">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+        <div className="font-bold w-full">
+          {isOpen ? (
+            <div className="font-poppins text-lg transition-all duration-500 bg-white text-gray-800 h-[200px] flex flex-col justify-between rounded-xl px-5 py-2">
+              <a href="#servicios" className="h-1/3 flex items-center">
+                <p>Servicios</p>
+              </a>
+              <a href="#nosotros" className="h-1/3 flex items-center">
+                <p>Nosotros</p>
+              </a>
+              <a href="#contacto" className="h-1/3 flex items-center">
+                <p>Contactanos</p>
+              </a>
+              <a href="#momentos" className="h-1/3 flex items-center">
+                <p>Momentos</p>
+              </a>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+      <div className="hidden lg:flex lg:p-4 lg:justify-center">
+        <div className="bg-white rounded-full h-10 p-7 w-[93%] flex justify-between items-center ">
+          <div>
+            <p className="text-2xl font-bold font-syne text-teal-300">
+              Clementina
+            </p>
+          </div>
+          <div className="flex gap-8 h-full items-center">
+            <a href="#servicios">
+              <p className="hover:text-[#128C7E] hover:scale-110 transition-all">Servicios</p>
             </a>
-            <a href="#servicios" className="h-1/3 flex items-center">
-              <p>Servicios</p>
+            <a href="#nosotros">
+              <p className="hover:text-[#128C7E] hover:scale-110 transition-all">Nosotros</p>
             </a>
-            <a href="#contacto" className="h-1/3 flex items-center">
-              <p>Contactanos</p>
+            <a href="#contacto">
+              <p className="hover:text-[#128C7E] hover:scale-110 transition-all">Contactanos</p>
             </a>
-            <a href="#momentos" className="h-1/3 flex items-center">
-              <p>Momentos</p>
+            <a href="#momentos">
+              <p className="hover:text-[#128C7E] hover:scale-110 transition-all">Momentos</p>
             </a>
           </div>
-        ) : (
-          <div></div>
-        )}
+        </div>
       </div>
     </div>
   );
